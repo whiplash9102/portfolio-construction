087255f347a43259d0614ffb3dda467fd1ea3c3c
whiplash9102 <183538437+whiplash9102@users.noreply.github.com>
2026-04-22 19:38:49 +0200

    new update in function for Present Value as well as CPPI

--- kit.py
+++ kit.py
@@ -3,6 +3,7 @@ import scipy.stats
 from scipy.stats import norm
 import numpy as np
 from scipy.optimize import minimize
+import math
 
 
 def drawdown(return_series: pd.Series):
@@ -249,6 +250,25 @@ def plot_ef2(npoints, er, cov, style=".-"):
     vols = [portfolio_vol(w, cov) for w in weights]
     ef = pd.DataFrame({"Returns": rets, "Volatility": vols})
     return ef.plot.line(x="Volatility", y="Returns", style=style)
+
+
+def discount(t, r):
+    """
+    Compute the price of pure discount bond that pay a dollar a time t, given interest rate r
+    """
+    return (1 + r) ** (-t)
+
+
+def pv(l, r):
+    """
+    Compute the present value of a sequence of liabilities
+    l is indexexed by the time, and the values are the amount of each liability
+    retun present value of the sequences
+    """
+
+    dates = l.index
+    discounts = discount(dates, r)
+    return (discounts * l).sum()
 
 
 def minimize_vol(target_return, er, cov):
@@ -502,3 +522,66 @@ def gbm(
     rets_plus_1[0] = 1
     ret_val = s_0 * pd.DataFrame(rets_plus_1).cumprod() if prices else rets_plus_1 - 1
     return ret_val
+
+
+def inst_to_ann(r):
+    """
+    Convert short rate to annualized rate
+    """
+    return np.expm1(r)
+
+
+def ann_to_inst(r):
+    """
+    Convert annualized rate to short rate
+    """
+    return np.log1p(r)
+
+
+def cir(
+    n_years=10, n_scenarios=1, a=0.05, b=0.03, sigma=0.05, steps_per_year=12, r_0=None
+):
+    """
+    Generate random interest rate evolution over time using the CIR model
+    b and r_0 are assumed to be the annualized rates, not the short rate
+    and the returned values are the annualized rates as well
+    """
+    if r_0 is None:
+        r_0 = b
+    r_0 = ann_to_inst(r_0)
+    dt = 1 / steps_per_year
+    num_steps = int(n_years * steps_per_year) + 1  # because n_years might be a float
+
+    shock = np.random.normal(0, scale=np.sqrt(dt), size=(num_steps, n_scenarios))
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+
+    ## For Price Generation
+    h = math.sqrt(a**2 + 2 * sigma**2)
+    prices = np.empty_like(shock)
+    ####
+
+    def price(ttm, r):
+        _A = (
+            (2 * h * math.exp((h + a) * ttm / 2))
+            / (2 * h + (h + a) * (math.exp(h * ttm) - 1))
+        ) ** (2 * a * b / sigma**2)
+        _B = (2 * (math.exp(h * ttm) - 1)) / (2 * h + (h + a) * (math.exp(h * ttm) - 1))
+        _P = _A * np.exp(-_B * r)
+        return _P
+
+    prices[0] = price(n_years, r_0)
+    ####
+
+    for step in range(1, num_steps):
+        r_t = rates[step - 1]
+        d_r_t = a * (b - r_t) * dt + sigma * np.sqrt(r_t) * shock[step]
+        rates[step] = abs(r_t + d_r_t)
+        # generate prices at time t as well ...
+        prices[step] = price(n_years - step * dt, rates[step])
+
+    rates = pd.DataFrame(data=inst_to_ann(rates), index=range(num_steps))
+    ### for prices
+    prices = pd.DataFrame(data=prices, index=range(num_steps))
+    ###
+    return rates, prices
